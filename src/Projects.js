6d7d2e85c80fceb8cf2045d74a6a8dc8cb8732ac
Odion Akhes <akhetuamhenodion@gmail.com>
2023-08-13 12:14:58 +0100

    first commit

---
+++ src/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useFetchProjects } from './fetchProjects'
+const Projects = () => {
+  const { loading, projects } = useFetchProjects()
+  console.log(loading,projects)
+  return (
+    <div>Projects</div>
+  )
+}
+
+export default Projects
